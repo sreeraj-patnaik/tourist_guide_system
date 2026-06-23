38d9a6bf67338aae05fd47ac615fb8259ea55460
sreeraj-patnaik <sreerajscs16225@gmail.com>
2026-06-23 20:59:08 +0530

    update

--- smart_tourist/settings.py
+++ smart_tourist/settings.py
@@ -85,8 +85,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'css', BASE_DIR / 'js']
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [BASE_DIR]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
